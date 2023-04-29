7731aaa7b65c521c59e152de99953078717f2291
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-29 16:11:08 +0200

    Add: Exercise-React-09

---
+++ Exercise-React-09/exercise-09/src/welcome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Age from "./age";
+
+const Welcome = (props) => {
+  const { name, age } = props;
+
+  if (age > 18) {
+    return (
+      <div>
+        <h1>
+          Welcome <strong>{props.name}</strong>
+        </h1>
+        <Age age={age} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          Welcome <strong>{props.name}</strong>
+        </h1>
+      </div>
+    );
+  }
+};
+
+export default Welcome;
+
+// This other method
+// { age > 18 &&
+//     <Age age={age} />
+//   }
