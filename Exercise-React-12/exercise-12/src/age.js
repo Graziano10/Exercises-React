18eba00c92d70caa6f735857beba7adcf2dac6d6
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-29 17:28:15 +0200

    Add: Exercise-React-12

---
+++ Exercise-React-12/exercise-12/src/age.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Age = (props) => {
+
+    const { age } = props;
+
+  return (
+    <div>
+        <p>Your age is {age}</p>
+    </div>
+  );
+};
+
+export default Age;
