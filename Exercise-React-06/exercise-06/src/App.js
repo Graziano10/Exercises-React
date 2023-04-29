0ce7b324607486d5f24ad87fbd2af9bec7b8aee0
Graziano Corineo <grazianocorineo01@gmail.com>
2023-04-29 15:06:18 +0200

    Add: Exercise-React-06

---
+++ Exercise-React-06/exercise-06/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import Welcome from "./welocme";
+
+function App() {
+  const ageW = {
+    age:25
+  };
+  return (
+    <div>
+      <Welcome age= '25' />
+  
+    </div>
+  );
+}
+
+export default App;
