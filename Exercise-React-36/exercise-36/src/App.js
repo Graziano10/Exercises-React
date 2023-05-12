34a42f6993f65a941d856c403fcb149c533d8e43
Graziano Corineo <grazianocorineo01@gmail.com>
2023-05-12 17:44:21 +0200

    Add: Exercise-React-36

---
+++ Exercise-React-36/exercise-36/src/App.js
@@ -0,0 +1,13 @@
+import { GithubUser } from "./GithubUser";
+
+
+
+function App() {
+  return (
+    <>
+      <GithubUser username="Luca" />
+    </>
+  );
+}
+
+export default App;
---
+++ Exercise-React-36/exercise-36/src/GithubUser.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export const GithubUser = ({ username }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios({
+          url: `https://api.github.com/users/${username}`,
+          method: "GET",
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [username]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p>Username: {data.login}</p>
+      <p>Location: {data.location}</p>
+    </div>
+  );
+};
